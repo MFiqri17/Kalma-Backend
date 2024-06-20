ad6a2252a9e367cf95e8aa09517c70311df3cf37
M. Fiqri Ananda <80850827+MFiqri17@users.noreply.github.com>
2024-06-20 17:23:35 +0700

    feat: add spesific path to mobile

--- src/utils/constant/path.ts
+++ src/utils/constant/path.ts
@@ -1,7 +1,7 @@
 const PATH = {
-  VERIFICATION_EMAIL: 'http://contohlink',
+  VERIFICATION_EMAIL: 'https://kalma-webapp.vercel.app/verify-email',
   VERIFICATION_EMAIL_WEB: 'https://kalma-webapp.vercel.app/verify-email',
-  FORGOT_PASSWORD: 'http://contohlink',
+  FORGOT_PASSWORD: 'https://kalma-webapp.vercel.app/reset-password',
   FORGOT_PASSWORD_WEB: 'https://kalma-webapp.vercel.app/reset-password',
 } as const;
 
